3c77010d0f68bede6fb7a095ea67657ae1bcb2ce
thekauer <thekauer@gmail.com>
2021-10-30 13:04:20 +0200

    refactor

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const HEIGHT = 60;
 const STRAIGHT = 0;
 const TURN = 1;
 const TRIPLET = 2;
+const NONE = 3;
 const PLAYER_COLORS = ["#cc0000", "#007acc", "#2ecb2d", "#ffe600"];
 let state = {
   page: "main",
@@ -132,146 +133,117 @@ function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function createElement(type, x, y, rotation, players = []) {
-  const ref = document.createElement("div");
-  return {
-    ref,
-    type,
-    rotation,
-    players,
-    getX: () => x,
-    getY: () => y,
-    setX: (newX) => {
-      x = newX;
-      element.style.left = `${x * WIDTH}px`;
-    },
-    setY: (newY) => {
-      y = newY;
-      element.style.top = `${y * HEIGHT}px`;
-    },
-  };
+class Element {
+  constructor(x, y, type, rotation, players = []) {
+    this.type = type;
+    this.ref = document.createElement("div");
+    if ([TURN, TRIPLET, STRAIGHT].includes(type)) {
+      this.ref.classList.add("door");
+      this.ref.style.backgroundImage = `url(./cell_${this.type}.svg)`;
+    }
+    this.ref.classList.add("cell");
+    this.setX(x);
+    this.setY(y);
+    this.setRotation(rotation);
+    this.players = players;
+  }
+
+  getX() {
+    return this.x;
+  }
+  getY() {
+    return this.y;
+  }
+  getRotation() {
+    return this.rotation;
+  }
+  setX(newX) {
+    this.x = newX;
+    this.ref.style.left = `${this.x * WIDTH}px`;
+  }
+  setY(newY) {
+    this.y = newY;
+    this.ref.style.top = `${this.y * HEIGHT}px`;
+  }
+  setRotation(newRotation) {
+    this.rotation = newRotation;
+    this.ref.style.transform = `rotate(${90 * this.rotation}deg)`;
+  }
 }
+
+function createElement(type, x, y, rotation, players = []) {
+  return new Element(x, y, type, rotation, players);
+}
+
+function getElement(x, y) {
+  return state.board.find((cell) => cell.getX() === x && cell.getY() === y);
+}
+
+const createStarterBoard = () => {
+  return [
+    createElement(TURN, 1, 1, RIGHT),
+    createElement(TURN, 1, 7, UP),
+    createElement(TURN, 7, 1, DOWN),
+    createElement(TURN, 7, 7, LEFT),
+
+    createElement(STRAIGHT, 7, 3, UP),
+    createElement(STRAIGHT, 7, 5, UP),
+
+    createElement(TRIPLET, 1, 3, UP),
+    createElement(TRIPLET, 1, 5, UP),
+
+    createElement(TRIPLET, 3, 7, LEFT),
+    createElement(TRIPLET, 5, 7, LEFT),
+
+    createElement(TRIPLET, 3, 1, RIGHT),
+    createElement(TRIPLET, 5, 1, RIGHT),
+
+    createElement(TRIPLET, 3, 3, UP),
+    createElement(TRIPLET, 5, 3, RIGHT),
+    createElement(TRIPLET, 3, 5, LEFT),
+    createElement(TRIPLET, 5, 5, DOWN),
+  ];
+};
+
+const fillBoard = () => {
+  const left = [13, 15, 6];
+  for (let i = 1; i < 8; i++) {
+    for (let j = 1; j < 8; j++) {
+      const elem = getElement(i, j);
+      if (elem && elem.type !== NONE) continue;
+
+      let type = randomBetween(0, 2);
+      while (left[type] === 0) {
+        type = randomBetween(0, 2);
+      }
+      left[type]--;
+      state.board.push(createElement(type, i, j, randomBetween(0, 3)));
+    }
+  }
+  state.board.push(createElement(left.indexOf(1), 0, 0, UP));
+};
+
 function initializeBoard() {
-  const creatBoard = () => {
-    const board = [];
-    for (let i = 0; i < 9; i++) {
-      board.push([]);
-      for (let j = 0; j < 9; j++) {
-        board[i].push({
-          x: i,
-          y: j,
-          players: [],
-        });
-      }
-    }
-    return board;
-  };
-  const createStarterBoard = (board) => {
-    board[1][1].type = TURN;
-    board[1][1].rotation = RIGHT;
-    board[1][7].type = TURN;
-    board[1][7].rotation = UP;
-    board[7][1].type = TURN;
-    board[7][1].rotation = DOWN;
-    board[7][7].type = TURN;
-    board[7][7].rotation = LEFT;
-
-    board[7][3].type = STRAIGHT;
-    board[7][3].rotation = UP;
-    board[7][5].type = STRAIGHT;
-    board[7][5].rotation = UP;
-
-    board[1][3].type = TRIPLET;
-    board[1][3].rotation = UP;
-    board[1][5].type = TRIPLET;
-    board[1][5].rotation = UP;
-
-    board[3][7].type = TRIPLET;
-    board[3][7].rotation = LEFT;
-    board[5][7].type = TRIPLET;
-    board[5][7].rotation = LEFT;
-
-    board[3][1].type = TRIPLET;
-    board[3][1].rotation = RIGHT;
-    board[5][1].type = TRIPLET;
-    board[5][1].rotation = RIGHT;
-
-    board[3][3].type = TRIPLET;
-    board[3][3].rotation = UP;
-    board[5][3].type = TRIPLET;
-    board[5][3].rotation = RIGHT;
-    board[3][5].type = TRIPLET;
-    board[3][5].rotation = LEFT;
-    board[5][5].type = TRIPLET;
-    board[5][5].rotation = DOWN;
-    return board;
-  };
-  const createDoor = (x, y, type) => {
-    const rotation = randomBetween(0, 3);
-    const door = {
-      x,
-      y,
-      type,
-      rotation,
-      players: [],
-    };
-    state.board[x][y] = door;
-  };
-  const fillBoard = () => {
-    const left = [13, 15, 6];
-    for (let i = 1; i < 8; i++) {
-      for (let j = 1; j < 8; j++) {
-        if (state.board[i][j].type !== undefined) continue;
-
-        let type = randomBetween(0, 2);
-        while (left[type] === 0) {
-          type = randomBetween(0, 2);
-        }
-        left[type]--;
-        createDoor(i, j, type);
-      }
-    }
-    createDoor(0, 0, left.indexOf(1));
-  };
-  const createRefs = () => {
-    state.board = state.board.map((row) =>
-      row.map((cell) => {
-        const door = document.createElement("div");
-        door.classList.add("cell");
-        if (cell.type !== undefined) {
-          door.classList.add("door");
-          door.style.transform = `rotate(${90 * cell.rotation}deg)`;
-          door.style.backgroundImage = `url(./cell_${cell.type}.svg)`;
-        }
-        door.style.left = `${cell.x * WIDTH}px`;
-        door.style.top = `${cell.y * HEIGHT}px`;
-        return { ...cell, ref: door };
-      })
-    );
-  };
-  const board = creatBoard();
-  state.board = createStarterBoard(board);
+  state.board = createStarterBoard();
   fillBoard();
-  createRefs();
 }
 
 function renderBoard() {
   const board = document.querySelector("#board");
-  state.board.map((row) =>
-    row.map((cell) => {
-      if (cell?.players.length > 0) {
-        cell.players?.forEach((player) => {
-          const playerDiv = document.createElement("div");
-          if (player.number === state.currentPlayer)
-            playerDiv.classList.add("active");
-          playerDiv.classList.add("player");
-          playerDiv.style.backgroundColor = player.color;
-          cell.ref.appendChild(playerDiv);
-        });
-      }
-      if (cell.ref && cell.type !== undefined) board.appendChild(cell.ref);
-    })
-  );
+
+  state.board.forEach((cell) => {
+    if (cell?.players.length > 0) {
+      cell.players?.forEach((player) => {
+        const playerDiv = document.createElement("div");
+        if (player.number === state.currentPlayer)
+          playerDiv.classList.add("active");
+        playerDiv.classList.add("player");
+        playerDiv.style.backgroundColor = player.color;
+        cell.ref.appendChild(playerDiv);
+      });
+    }
+    if (cell.ref && cell.type !== undefined) board.appendChild(cell.ref);
+  });
 }
 
 function initializePlayers() {
@@ -290,27 +262,35 @@ function initializePlayers() {
       x,
       y,
     };
-    if (state.board[x][y].x === undefined) {
-      state.board[x][y] = { x, y, players: [] };
+    const cell = state.board.find((cell) => cell.x === x && cell.y === y);
+    if (cell) {
+      cell.players.push(player);
+    } else {
+      state.board.push(createElement(NONE, x, y, UP, [player]));
     }
-    state.board[x][y].players.push(player);
   }
   state.currentPlayer = 0;
 }
 
 function shift(x, y) {
-  for (let i = 1; i < 8; i++) {
-    if (x === 0) {
-      state.board[i][y].ref.style.left = `${(i + 1) * HEIGHT}px`;
-    }
-    if (x === 8) {
-      state.board[i][y].ref.style.left = `${(i - 1) * HEIGHT}px`;
-    }
-    if (y === 0) {
-      state.board[x][i].ref.style.top = `${(i + 1) * HEIGHT}px`;
-    }
-    if (y === 8) {
-      state.board[x][i].ref.style.top = `${(i - 1) * HEIGHT}px`;
-    }
+  if (x === 0) {
+    state.board
+      .filter((cell) => cell.getY() === y)
+      .forEach((cell) => cell.setX(cell.getX() + 1));
+  }
+  if (x === 8) {
+    state.board
+      .filter((cell) => cell.getY() === y)
+      .forEach((cell) => cell.setX(cell.getX() - 1));
+  }
+  if (y === 0) {
+    state.board
+      .filter((cell) => cell.getX() === x)
+      .forEach((cell) => cell.setY(cell.getY() + 1));
+  }
+  if (y === 8) {
+    state.board
+      .filter((cell) => cell.getX() === x)
+      .forEach((cell) => cell.setY(cell.getY() - 1));
   }
 }
